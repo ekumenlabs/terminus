57e3b65a839fa044a61f1143306acb6637c7f6e8
Andres Fortier <andres@creativa77.com>
2016-09-27 18:58:28 -0300

    Initial (hardcoded) implementation

---
+++ terminus/builders/procedural_city_builder.py
@@ -0,0 +1,44 @@
+from models.city import City
+from models.road import Street, Trunk
+from models.point import Point
+from models.block import Block
+from models.building import Building
+from models.ground_plane import GroundPlane
+
+from procedural_city.vertex import Vertex
+
+import sys
+import pickle
+import os
+import re
+
+class ProceduralCityBuilder(object):
+    def __init__(self, filename):
+        self.filename = filename
+
+    def get_city(self):
+        city = City()
+        ratio = 10
+        print("Parsing file...")
+        vertex_list = self._parse_file()
+        print("Building streets...")
+        for vertex in vertex_list:
+            for neighbour in vertex.neighbours:
+                road = Street()
+                road.add_segment(Point(vertex.coords[0]*ratio, vertex.coords[1]*ratio, 0))
+                road.add_segment(Point(neighbour.coords[0]*ratio, neighbour.coords[1]*ratio, 0))
+                city.add_road(road)
+        city.set_ground_plane(GroundPlane(100, Point(0,0,0)))
+        return city
+
+    def _parse_file(self):
+        with open(self.filename, 'rb') as f:
+            # TODO: We should read/pre-process this lazily, no point in loading
+            # the whole thing in memory.
+            # Here we do the hacky conversion, from the Vertex class dumped in
+            # procedural_city_generation package to our Vertex class use by this
+            # builder.
+            originalPackage = 'procedural_city_generation.roadmap.Vertex'
+            builderPackage = 'builders.procedural_city.vertex'
+            contents = f.read().replace(originalPackage, builderPackage)
+            return pickle.loads(contents)
--- terminus/run_generator.py
+++ terminus/run_generator.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python
-from builders.simple_city_builder import SimpleCityBuilder
 
-city = SimpleCityBuilder().get_city()
+# We should take as parameters the builder class and the required parameters
+#Old:
+#from builders.simple_city_builder import SimpleCityBuilder
+#city = SimpleCityBuilder().get_city()
+
+# New:
+from builders.procedural_city_builder import ProceduralCityBuilder
+path = "/home/andres/Creativa77/OSRF/procedural_city_generation/procedural_city_generation/temp/mycity"
+city = ProceduralCityBuilder(path).get_city()
 
 with open("generated_worlds/city.world", "w") as f:
     f.write(city.to_sdf())
