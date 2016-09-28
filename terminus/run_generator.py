0ea99f3cd17413395fab3d47c2887572de7c4462
Andres Fortier <andres@creativa77.com>
2016-09-28 16:16:43 -0300

    Make the runner configurable

--- terminus/run_generator.py
+++ terminus/run_generator.py
@@ -1,14 +1,45 @@
 #!/usr/bin/python
 
-# We should take as parameters the builder class and the required parameters
-#Old:
-#from builders.simple_city_builder import SimpleCityBuilder
-#city = SimpleCityBuilder().get_city()
+from builders import *
 
-# New:
-from builders.procedural_city_builder import ProceduralCityBuilder
-path = "/home/andres/Creativa77/OSRF/procedural_city_generation/procedural_city_generation/temp/mycity"
-city = ProceduralCityBuilder(path).get_city()
+import argparse
+import sys
 
-with open("generated_worlds/city.world", "w") as f:
+parser = argparse.ArgumentParser()
+
+# The builder class
+parser.add_argument('-b',
+                    '--builder',
+                    required=True,
+                    help='class name of the builder to invoke')
+# The output file
+parser.add_argument('-d',
+                    '--destination',
+                    default='generated_worlds/city.world',
+                    help='destination file name')
+# Extra named parameters passed to the builder when creating it
+parser.add_argument('-p',
+                    '--parameters',
+                    nargs='*',
+                    help='extra parameters to pass to the builder. Must be formatted as <key>=<value> pairs')
+
+arguments = parser.parse_args()
+
+# Get the file name to write to
+destination_file = arguments.destination
+
+# Get the class of the builder to use
+builder_class = getattr(sys.modules[__name__], arguments.builder)
+
+# Get the remaining parameters to pass to the builder as <key>=<value> pairs
+# as a dictionary
+builder_parameters = dict(pair.split('=', 1) for pair in arguments.parameters)
+
+# Create the builder instance. Unpack the parameter dictionary to be used as
+# keyword parameters
+builder = builder_class(**builder_parameters);
+
+city = builder.get_city()
+
+with open(destination_file, "w") as f:
     f.write(city.to_sdf())
