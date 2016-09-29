f29869fa7bc8e07845fa0d2bfd084fc41c7e3667
Andres Fortier <andres@creativa77.com>
2016-09-29 18:59:11 -0300

    Avoid duplicated street segments

--- terminus/builders/procedural_city_builder.py
+++ terminus/builders/procedural_city_builder.py
@@ -1,4 +1,5 @@
 from geometry.point import Point
+from geometry.line_segment import LineSegment
 from models.city import City
 from models.road import Street, Trunk
 from models.block import Block
@@ -18,16 +19,29 @@ class ProceduralCityBuilder(object):
         city = City()
         ratio = 10
         vertex_list = self._parse_file()
-        for vertex in vertex_list:
-            for neighbour in vertex.neighbours:
-                road = Street()
-                road.add_segment(Point(vertex.coords[0]*ratio,
-                                       vertex.coords[1]*ratio))
-                road.add_segment(Point(neighbour.coords[0]*ratio,
-                                       neighbour.coords[1]*ratio))
-                city.add_road(road)
+        segments = self._build_unique_street_segments(vertex_list, ratio)
+        for segment in segments:
+            road = Street()
+            road.add_segment(segment.start)
+            road.add_segment(segment.end)
+            city.add_road(road)
         city.set_ground_plane(GroundPlane(100, Point(0, 0)))
         return city
+
+    def _build_unique_street_segments(self, vertex_list, ratio):
+        """For each street segment there are two vertex in the list, with
+           the coords and neighbour exchanged (like a two-way street). We
+           don't support street directions so far, so we just get rid of
+           the duplicate road segments"""
+        segments = set()
+        for vertex in vertex_list:
+            for neighbour in vertex.neighbours:
+                start = Point(vertex.coords[0]*ratio,
+                              vertex.coords[1]*ratio)
+                end = Point(neighbour.coords[0]*ratio,
+                            neighbour.coords[1]*ratio)
+                segments.add(LineSegment(start, end))
+        return segments
 
     def _parse_file(self):
         with open(self.filename, 'rb') as f:
--- terminus/geometry/point.py
+++ terminus/geometry/point.py
@@ -4,3 +4,12 @@ class Point():
         self.x = x
         self.y = y
         self.z = z
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y and self.z == other.z
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        return hash(tuple(sorted(self.__dict__.items())))
