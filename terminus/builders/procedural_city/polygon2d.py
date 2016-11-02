590f5da365bf2a2cfb87ad1c5525f920079dc0b1
Andres Fortier <andres@creativa77.com>
2016-11-02 12:16:59 -0300

    Fix build

--- terminus/builders/procedural_city/polygon2d.py
+++ terminus/builders/procedural_city/polygon2d.py
@@ -13,7 +13,7 @@ class Edge(object):
         self.vertices = np.array((vertex1, vertex2))
         self.dir_vector = vertex2 - vertex1
         self.length = np.linalg.norm(self.dir_vector)
-        v = self.dir_vector/self.length
+        v = self.dir_vector / self.length
         self.n = np.array((v[1], -v[0]))
 
     def __getitem__(self, i):
@@ -31,11 +31,11 @@ class Polygon2D(object):
         else:
             self.vertices = in_list
             self.edges = [Edge(v1, v2) for v1, v2 in
-                          zip(in_list, in_list[1:]+[in_list[0]])]
+                          zip(in_list, in_list[1:] + [in_list[0]])]
         self.poly_type = poly_type
 
     def get_vertices_as_points(self, ratio):
-        return list((arr[0]*ratio, arr[1]*ratio, 0) for arr in self.vertices)
+        return list((arr[0] * ratio, arr[1] * ratio, 0) for arr in self.vertices)
 
     def is_lot(self):
         return self.poly_type == 'lot'
--- terminus/builders/simple_city_builder.py
+++ terminus/builders/simple_city_builder.py
@@ -22,10 +22,10 @@ class SimpleCityBuilder(object):
         return city
 
     def _create_ground_plane(self, city, size):
-        ground_plane_size = size*100
+        ground_plane_size = size * 100
         ground_plane = GroundPlane(ground_plane_size,
-                                   Point(ground_plane_size/2,
-                                         ground_plane_size/2,
+                                   Point(ground_plane_size / 2,
+                                         ground_plane_size / 2,
                                          0),
                                    'ground_plane')
         city.set_ground_plane(ground_plane)
@@ -34,46 +34,46 @@ class SimpleCityBuilder(object):
         # West-East streets
         for x in range(1, size):
             road = Street()
-            road.add_point(Point(0, x*100, 0))
-            road.add_point(Point(100 * size, x*100, 0))
+            road.add_point(Point(0, x * 100, 0))
+            road.add_point(Point(100 * size, x * 100, 0))
             city.add_road(road)
 
         # North-South streets
         for x in range(1, size):
             road = Street()
-            road.add_point(Point(x*100, 0, 0))
-            road.add_point(Point(x*100, 100 * size, 0))
+            road.add_point(Point(x * 100, 0, 0))
+            road.add_point(Point(x * 100, 100 * size, 0))
             city.add_road(road)
 
         # Diagonal streets
         road = Street()
         road.add_point(Point(0, 0, 0))
-        road.add_point(Point(100*size, 100*size, 0))
+        road.add_point(Point(100 * size, 100 * size, 0))
         city.add_road(road)
         road = Street()
-        road.add_point(Point(100*size, 0, 0))
-        road.add_point(Point(0, 100*size, 0))
+        road.add_point(Point(100 * size, 0, 0))
+        road.add_point(Point(0, 100 * size, 0))
         city.add_road(road)
 
     def _create_surrounding_ring_road(self, city, size):
         ring_road_1 = Street('RingRoad1')
         ring_road_1.add_point(Point(-2.5, 0, 0))
-        ring_road_1.add_point(Point(size*100+2.5, 0, 0))
+        ring_road_1.add_point(Point(size * 100 + 2.5, 0, 0))
         city.add_road(ring_road_1)
 
         ring_road_2 = Street('RingRoad2')
-        ring_road_2.add_point(Point(size*100, 0, 0))
-        ring_road_2.add_point(Point(size*100, size*100, 0))
+        ring_road_2.add_point(Point(size * 100, 0, 0))
+        ring_road_2.add_point(Point(size * 100, size * 100, 0))
         city.add_road(ring_road_2)
 
         ring_road_3 = Street('RingRoad3')
-        ring_road_3.add_point(Point(-2.5, size*100, 0))
-        ring_road_3.add_point(Point(size*100+2.5, size*100, 0))
+        ring_road_3.add_point(Point(-2.5, size * 100, 0))
+        ring_road_3.add_point(Point(size * 100 + 2.5, size * 100, 0))
         city.add_road(ring_road_3)
 
         ring_road_4 = Street('RingRoad4')
         ring_road_4.add_point(Point(0, 0, 0))
-        ring_road_4.add_point(Point(0, size*100, 0))
+        ring_road_4.add_point(Point(0, size * 100, 0))
         city.add_road(ring_road_4)
 
     def _create_blocks(self, city, size):
@@ -121,19 +121,19 @@ class SimpleCityBuilder(object):
         block = Block(Point(2.5, 200 + 2.5, 0), vertices)
         city.add_block(block)
 
-        vertices = [Point(0, 0, 0), Point(88, 0, 0), Point(88/2, -44, 0)]
+        vertices = [Point(0, 0, 0), Point(88, 0, 0), Point(88 / 2, -44, 0)]
         block = Block(Point(100 + 6, 200 - 2.5, 0), vertices)
         city.add_block(block)
 
-        vertices = [Point(0, 0, 0), Point(88, 0, 0), Point(88/2, 44, 0)]
+        vertices = [Point(0, 0, 0), Point(88, 0, 0), Point(88 / 2, 44, 0)]
         block = Block(Point(100 + 6, 100 + 2.5, 0), vertices)
         city.add_block(block)
 
-        vertices = [Point(0, 0, 0), Point(0, 88, 0), Point(44, 88/2, 0)]
+        vertices = [Point(0, 0, 0), Point(0, 88, 0), Point(44, 88 / 2, 0)]
         block = Block(Point(100 + 2.5, 100 + 6, 0), vertices)
         city.add_block(block)
 
-        vertices = [Point(0, 0, 0), Point(0, 88, 0), Point(-44, 88/2, 0)]
+        vertices = [Point(0, 0, 0), Point(0, 88, 0), Point(-44, 88 / 2, 0)]
         block = Block(Point(200 - 2.5, 100 + 6, 0), vertices)
         city.add_block(block)
 
@@ -142,8 +142,8 @@ class SimpleCityBuilder(object):
             for y in range(size):
                 for block_x in range(6):
                     for block_y in range(6):
-                        pos = Point(x*100+block_x*15+12,
-                                    y*100+block_y*15+12, 0)
+                        pos = Point(x * 100 + block_x * 15 + 12,
+                                    y * 100 + block_y * 15 + 12, 0)
                         if abs(pos.y + pos.x - 300.0) > 12.0 and\
                                 abs(pos.y - pos.x) > 12.0:
                             building = Building(pos, 10, 40)
