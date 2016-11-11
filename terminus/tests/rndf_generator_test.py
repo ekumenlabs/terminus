be6374c599c25d68b432f1e8c6c262e4debb0496
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2016-11-11 18:06:40 -0300

    Add L junction test case.

--- terminus/tests/rndf_generator_test.py
+++ terminus/tests/rndf_generator_test.py
@@ -71,6 +71,7 @@ class RNDFGeneratorTest(unittest.TestCase):
         """
         city = City("Cross")
         junction = JunctionNode.on(0, 0)
+        self.maxDiff = None
 
         s1 = Street.from_nodes([
             SimpleNode.on(-1000, 0),
@@ -118,6 +119,60 @@ class RNDFGeneratorTest(unittest.TestCase):
         2.1.2\t45.000045\t65.000000
         2.1.3\t44.999955\t65.000000
         2.1.4\t44.991017\t65.000000
+        end_lane
+        end_segment
+        end_file""")
+
+    def test_L_junction(self):
+
+        """
+             (0,1)
+               +  (1,0)
+        """
+        city = City("LCross")
+        junction = JunctionNode.on(0,0)
+
+        s1 = Street.from_nodes([
+            SimpleNode.on(0, 1000),
+            junction
+        ])
+        s1.name = "s1"
+
+        s2 = Street.from_nodes([
+            junction,
+            SimpleNode.on(1000, 0)
+        ])
+        s2.name = "s2"
+        city.add_road(s1)
+        city.add_road(s2)
+
+        self._generate_rndf(city)
+        self._assert_contents_are("""
+        RNDF_name\tLCross
+        num_segments\t2
+        num_zones\t0
+        format_version\t1.0
+        segment\t1
+        num_lanes\t1
+        segment_name\ts1
+        lane\t1.1
+        num_waypoints\t3
+        lane_width\t5
+        exit\t1.1.2\t2.1.2
+        1.1.1\t45.008983\t65.000000
+        1.1.2\t45.000045\t65.000000
+        1.1.3\t45.000000\t65.000000
+        end_lane
+        end_segment
+        segment\t2
+        num_lanes\t1
+        segment_name\ts2
+        lane\t2.1
+        num_waypoints\t3
+        lane_width\t5
+        2.1.1\t45.000000\t65.000000
+        2.1.2\t45.000000\t64.999914
+        2.1.3\t45.000000\t64.982900
         end_lane
         end_segment
         end_file""")
