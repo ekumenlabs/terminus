b9d434330921af607b45e088905e25f46b1ff40e
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2016-11-16 13:52:12 -0300

    Add Y junction test cases.

--- terminus/tests/rndf_generator_test.py
+++ terminus/tests/rndf_generator_test.py
@@ -13,6 +13,10 @@ import textwrap
 
 class RNDFGeneratorTest(unittest.TestCase):
 
+    def setUp(self):
+        # print full diff in test output
+        self.maxDiff = None
+
     def _generate_rndf(self, city):
         self.generator = RNDFGenerator(city, Point(45, 65))
         self.generated_contents = self.generator.generate()
@@ -71,7 +75,6 @@ class RNDFGeneratorTest(unittest.TestCase):
         """
         city = City("Cross")
         junction = JunctionNode.on(0, 0)
-        self.maxDiff = None
 
         s1 = Street.from_nodes([
             SimpleNode.on(-1000, 0),
@@ -173,6 +176,156 @@ class RNDFGeneratorTest(unittest.TestCase):
         2.1.1\t45.000000\t65.000000
         2.1.2\t45.000000\t64.999914
         2.1.3\t45.000000\t64.982900
+        end_lane
+        end_segment
+        end_file""")
+
+    def test_Y_junction_one_to_many(self):
+
+        """
+                  (0,1)
+                    +
+            (-1,-1)   (1,-1)
+        """
+        city = City("YCross")
+        junction = JunctionNode.on(0,0)
+
+        s1 = Street.from_nodes([
+            SimpleNode.on(0, 1000),
+            junction
+        ])
+        s1.name = "s1"
+
+        s2 = Street.from_nodes([
+            junction,
+            SimpleNode.on(-1000, -1000)
+        ])
+        s2.name = "s2"
+
+        s3 = Street.from_nodes([
+            junction,
+            SimpleNode.on(1000, -1000)
+        ])
+        s3.name = "s3"
+
+        city.add_road(s1)
+        city.add_road(s2)
+        city.add_road(s3)
+
+        self._generate_rndf(city)
+        self._assert_contents_are("""
+        RNDF_name\tYCross
+        num_segments\t3
+        num_zones\t0
+        format_version\t1.0
+        segment\t1
+        num_lanes\t1
+        segment_name\ts1
+        lane\t1.1
+        num_waypoints\t3
+        lane_width\t5
+        exit\t1.1.2\t2.1.2
+        exit\t1.1.2\t3.1.2
+        1.1.1\t45.008983\t65.000000
+        1.1.2\t45.000045\t65.000000
+        1.1.3\t45.000000\t65.000000
+        end_lane
+        end_segment
+        segment\t2
+        num_lanes\t1
+        segment_name\ts2
+        lane\t2.1
+        num_waypoints\t3
+        lane_width\t5
+        2.1.1\t45.000000\t65.000000
+        2.1.2\t44.999968\t65.000060
+        2.1.3\t44.991017\t65.017100
+        end_lane
+        end_segment
+        segment\t3
+        num_lanes\t1
+        segment_name\ts3
+        lane\t3.1
+        num_waypoints\t3
+        lane_width\t5
+        3.1.1\t45.000000\t65.000000
+        3.1.2\t44.999968\t64.999940
+        3.1.3\t44.991017\t64.982900
+        end_lane
+        end_segment
+        end_file""")
+
+    def test_Y_junction_many_to_one(self):
+
+        """
+                  (0,1)
+                    +
+            (-1,-1)   (1,-1)
+        """
+        city = City("YCross")
+        junction = JunctionNode.on(0,0)
+
+        s1 = Street.from_nodes([
+            junction,
+            SimpleNode.on(0, 1000)
+        ])
+        s1.name = "s1"
+
+        s2 = Street.from_nodes([
+            SimpleNode.on(-1000, -1000),
+            junction
+        ])
+        s2.name = "s2"
+
+        s3 = Street.from_nodes([
+            SimpleNode.on(1000, -1000),
+            junction
+        ])
+        s3.name = "s3"
+
+        city.add_road(s1)
+        city.add_road(s2)
+        city.add_road(s3)
+
+        self._generate_rndf(city)
+        self._assert_contents_are("""
+        RNDF_name\tYCross
+        num_segments\t3
+        num_zones\t0
+        format_version\t1.0
+        segment\t1
+        num_lanes\t1
+        segment_name\ts1
+        lane\t1.1
+        num_waypoints\t3
+        lane_width\t5
+        1.1.1\t45.000000\t65.000000
+        1.1.2\t45.000045\t65.000000
+        1.1.3\t45.008983\t65.000000
+        end_lane
+        end_segment
+        segment\t2
+        num_lanes\t1
+        segment_name\ts2
+        lane\t2.1
+        num_waypoints\t3
+        lane_width\t5
+        exit\t2.1.2\t1.1.2
+        2.1.1\t44.991017\t65.017100
+        2.1.2\t44.999968\t65.000060
+        2.1.3\t45.000000\t65.000000
+        end_lane
+        end_segment
+        segment\t3
+        num_lanes\t1
+        segment_name\ts3
+        lane\t3.1
+        num_waypoints\t3
+        lane_width\t5
+        exit\t3.1.2\t1.1.2
+        3.1.1\t44.991017\t64.982900
+        3.1.2\t44.999968\t64.999940
+        3.1.3\t45.000000\t65.000000
         end_lane
         end_segment
         end_file""")
