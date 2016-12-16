e1570fb9932a6d07146ded1f9907b51551f44429
Ernesto Corbellini <ecorbellini@ekumenlabs.com>
2016-12-16 18:15:20 -0300

    Load building information from OSM.

--- terminus/models/building.py
+++ terminus/models/building.py
@@ -1,19 +1,33 @@
 from city_model import CityModel
+from geometry.point import Point
 
 
+# TODO: separate concepts of origin and translation vector for the pose.
 class Building(CityModel):
-    def __init__(self, origin, size, height, name=None):
+    def __init__(self, origin, vertices, height=10, name=None):
         """
-        Generates a square building of base (size * size) and the given height,
-        located at position 'origin'.
+        Generates a building with a base shape given by the vertices list
+        and extruded the given height, located at position 'origin'.
         """
         super(Building, self).__init__(name)
         self.origin = origin
-        self.size = size
+        self.vertices = vertices
         self.height = height
 
-    def box_base(self):
-        return self.origin.z + self.height / 2
+    @staticmethod
+    def square(origin, size, height):
+        """
+        Generate a square building of the given lateral size, height and
+        centered in the given position.
+        """
+        w = size / 2.0
+        vertices = [
+            Point(w, w, 0),
+            Point(-w, w, 0),
+            Point(-w, -w, 0),
+            Point(w, -w, 0)
+        ]
+        return Building(origin, vertices, height)
 
     def accept(self, generator):
         generator.start_building(self)
