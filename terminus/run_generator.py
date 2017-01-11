42b5074f7bcf1d9c94862653fa6ebf584a973db2
Andres Fortier <andres@creativa77.com>
2017-01-11 20:02:52 -0300

    Add minimal feedback

--- terminus/run_generator.py
+++ terminus/run_generator.py
@@ -10,6 +10,16 @@ import argparse
 import sys
 import os
 
+import logging
+
+logger = logging.getLogger('')
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+logger.handlers = []
+logger.addHandler(handler)
+
 # For the time being we use an arbitrary (lat,lon) pair as the origin
 RNDF_ORIGIN = (10, 65)
 
@@ -63,11 +73,15 @@ if 'size' in builder_parameters:
 # keyword parameters
 builder = builder_class(**builder_parameters)
 
+logger.info("Building city using {0}".format(builder_class.__name__))
+
 city = builder.get_city()
 
+logger.info("Generating Gazebo 7 SDF")
 sdf_generator = SDFGeneratorGazebo7(city)
 sdf_generator.write_to(destination_sdf_7_file)
 
+logger.info("Generating Gazebo 8 SDF")
 # We are using a path that suits the Gazebo 8 plugin. This will change once
 # https://bitbucket.org/JChoclin/rndf_gazebo_plugin/issues/53/rndf-file-path-in-world-file
 # is fixed
@@ -78,8 +92,10 @@ sdf_generator = SDFGeneratorGazebo8(city, RNDF_ORIGIN, '../example/' + rndf_file
 sdf_generator.write_to(destination_sdf_8_file)
 
 if arguments.debug:
+    logger.info("Generating street plot")
     street_plot_generator = StreetPlotGenerator(city)
     street_plot_generator.write_to(destination_street_plot_file)
 
+logger.info("Generating RNDF file")
 rndf_generator = RNDFGenerator(city, Point(RNDF_ORIGIN[0], RNDF_ORIGIN[1]))
 rndf_generator.write_to(destination_rndf_file)
