a5fddd5245d508a54d2c78d51874b36046209264
Andres Fortier <andres@creativa77.com>
2017-01-30 19:00:26 -0300

    Tweak street plot generator

--- terminus/generators/street_plot_generator.py
+++ terminus/generators/street_plot_generator.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 class StreetPlotGenerator(CityVisitor):
 
     def write_to(self, destination_file):
-        plt.figure(figsize=(20, 20), dpi=100)
+        plt.figure(figsize=(20, 20), dpi=150)
         self.run()
-        plt.savefig(destination_file, dpi=100)
+        plt.savefig(destination_file, dpi=150)
 
     def draw_lane(self, lane):
         x = []
@@ -25,4 +25,4 @@ class StreetPlotGenerator(CityVisitor):
     def start_trunk(self, trunk):
         for lane in trunk.get_lanes():
             line = self.draw_lane(lane)
-            plt.setp(line, linewidth=2)
+            plt.setp(line, linewidth=1)
