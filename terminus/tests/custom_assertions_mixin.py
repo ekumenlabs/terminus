d0ef435559edf470d5e8ebd6035bb4bcee59a0d9
Andres Fortier <andres@creativa77.com>
2017-04-03 12:39:23 -0300

    Kickoff arc implementation

---
+++ terminus/tests/custom_assertions_mixin.py
@@ -0,0 +1,110 @@
+"""
+Copyright (C) 2017 Open Source Robotics Foundation
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from geometry.point import Point
+from geometry.line_segment import LineSegment
+from geometry.arc import Arc
+
+
+class CustomAssertionsMixin(object):
+
+    def assertAlmostEqual(self, first, second, places=7, msg=None, delta=None):
+        first_type = type(first)
+        if first_type is int or first_type is float:
+            return super(CustomAssertionsMixin, self).assertAlmostEqual(first, second, places=places, msg=msg, delta=delta)
+        elif first_type is Point:
+            return self.assertPointAlmostEqual(first, second, places=places, msg=msg)
+        elif first_type is Arc:
+            return self.assertArcAlmostEqual(first, second, places=places, msg=msg)
+        elif first_type is LineSegment:
+            return self.assertLineSegmentAlmostEqual(first, second, places=places, msg=msg)
+        elif first_type is list:
+            return self.assertListAlmostEqual(first, second, places=places, msg=msg)
+        else:
+            return self.assertEquals(first, second, msg=msg)
+
+    def assertPointAlmostEqual(self, point1, point2, places=7, msg=None):
+        if msg:
+            x_message = msg
+            y_message = msg
+        else:
+            x_message = "{0} != {1} within {2} places (x coordinate)".format(point1, point2, places)
+            y_message = "{0} != {1} within {2} places (y coordinate)".format(point1, point2, places)
+        self.assertAlmostEqual(point1.x, point2.x, places=places, msg=x_message)
+        self.assertAlmostEqual(point1.y, point2.y, places=places, msg=y_message)
+
+    def assertArcAlmostEqual(self, arc1, arc2, places=7, msg=None):
+        if msg:
+            start_point_msg = msg
+            theta_msg = msg
+            radius_msg = msg
+            angular_length_msg = msg
+        else:
+            base_message = "{0} != {1} within {2} places ({3})".format(arc1, arc2, places, '{0}')
+            start_point_msg = base_message.format('start_point')
+            theta_msg = base_message.format('theta')
+            radius_msg = base_message.format('radius')
+            angular_length_msg = base_message.format('angular_length')
+        self.assertPointAlmostEqual(arc1.start_point(),
+                                    arc2.start_point(),
+                                    places=places,
+                                    msg=start_point_msg)
+        self.assertAlmostEqual(arc1.theta(),
+                               arc2.theta(),
+                               places=places,
+                               msg=theta_msg)
+        self.assertAlmostEqual(arc1.radius(),
+                               arc2.radius(),
+                               places=places,
+                               msg=radius_msg)
+        self.assertAlmostEqual(arc1.angular_length(),
+                               arc2.angular_length(),
+                               msg=angular_length_msg)
+
+    def assertLineSegmentAlmostEqual(self, segment1, segment2, places=7, msg=None):
+        if msg:
+            start_point_msg = msg
+            end_point_msg = msg
+        else:
+            base_message = "{0} != {1} within {2} places ({3})".format(segment1, segment2, places, '{0}')
+            start_point_msg = base_message.format('start_point')
+            end_point_msg = base_message.format('end_point')
+        self.assertPointAlmostEqual(segment1.start_point(),
+                                    segment2.start_point(),
+                                    places=places,
+                                    msg=start_point_msg)
+        self.assertPointAlmostEqual(segment1.end_point(),
+                                    segment2.end_point(),
+                                    places=places,
+                                    msg=end_point_msg)
+
+    def assertListAlmostEqual(self, list1, list2, places=7, msg=None):
+        if len(list1) != len(list2):
+            if msg:
+                message = msg
+            else:
+                message = "Lists have different sizes\n{0}\n{1}".format(list1, list2)
+            raise AssertionError(message)
+        for index, (element1, element2) in enumerate(zip(list1, list2)):
+            try:
+                self.assertAlmostEqual(element1, element2, places)
+            except AssertionError as e:
+                if msg:
+                    message = msg
+                else:
+                    message = "Lists differ in index {0}:\n\n{1}\n\n{2}\n{3}".format(index, e.args[0], list1, list2)
+                    e.args = (message,)
+                    raise
